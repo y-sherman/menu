669d49f7a90f66a4e5164f65d8a8a56eb3f6e05d
Priva Milikowsky <privamili@gmail.com>
2024-06-24 18:47:41 -0400

    added front end design and functions

--- src/Pages/Add.jsx
+++ src/Pages/Add.jsx
@@ -1,8 +1,46 @@
+import React, { useState } from 'react';
+import { Container, Form, Button, ListGroup} from 'react-bootstrap';
+
+
 export default function Add(){
-    return(
-        <>
-            <h1>This is the Add Page</h1>
-        </>
-        
-    )
+   // create list that holds all the menus
+const menuList = () => {
+    const [menus, setMenus] = useState([]);
+    const [menuName, setMenuName] = useState('');
+
+    const handleInputChange = (e) => {
+        setItemName(e.target.value);
+    };
+
+    const addMenu = () => {
+        if (menuName.trim() !== '') {
+            setMenus([...menus, menuName]);
+            setMenuName('');
+        };
+    };
+
+    return (
+        <Container>
+            <h1 className = "mt-4">Add a new menu</h1>
+
+            <Form className = "mt-4">
+                <Form.Group>
+                    <Form.Control 
+                        type = "text"
+                        placeholder = "Enter the name of your menu"
+                        value = {menuName}
+                        onChange = {handleInputChange}
+                    />
+                </Form.Group>
+                <Button variant = "primary" onClick = {addMenu}>Add Menu</Button>
+            </Form>
+
+            <ListGroup className = "mt-4">
+                {menus.map((item, index) => (
+                    <ListGroup.Item key = {index}> {item} </ListGroup.Item>
+                ))}
+            </ListGroup>
+        </Container>
+    );  
+};
 }
