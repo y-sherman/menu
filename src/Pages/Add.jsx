ae1b4c5cffaf7f6b3c2d15d08274b205ecc787d7
Priva Milikowsky <privamili@gmail.com>
2024-06-24 22:19:39 -0400

    fixing buttons

--- src/Pages/Add.jsx
+++ src/Pages/Add.jsx
@@ -1,46 +1,70 @@
 import React, { useState } from 'react';
 import { Container, Form, Button, ListGroup} from 'react-bootstrap';
 
-
-export default function Add(){
-   // create list that holds all the menus
-const menuList = () => {
+const MenuList = () => {
     const [menus, setMenus] = useState([]);
     const [menuName, setMenuName] = useState('');
+    const [menuDescription, setMenuDescription] = useState('')
 
-    const handleInputChange = (e) => {
-        setItemName(e.target.value);
+    const handleNameChange = (e) => {
+        setMenuName(e.target.value);
+    };
+
+    const handleDescriptionChange = (e) => {
+        setMenuDescription(e.target.value);
     };
 
     const addMenu = () => {
-        if (menuName.trim() !== '') {
-            setMenus([...menus, menuName]);
-            setMenuName('');
-        };
+        if (menuName.trim() !== '' && menuDescription.trim() !== '') {
+            const newMenu = {
+                name: menuName,
+                description: menuDescription
+            };
+            
+            setMenus([...menus, newMenu]);
+            setMenuName('');    //clears input after adding an item
+            setMenuDescription('');
+        }
     };
 
-    return (
+
+    return (                                                
         <Container>
-            <h1 className = "mt-4">Add a new menu</h1>
+            <h1 className = "mt-4">Add a new menu item</h1>
 
             <Form className = "mt-4">
                 <Form.Group>
+                    <Form.Label>Menu Name</Form.Label>
                     <Form.Control 
                         type = "text"
                         placeholder = "Enter the name of your menu"
                         value = {menuName}
-                        onChange = {handleInputChange}
+                        onChange = {handleNameChange}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Menu Description</Form.Label>
+                    <Form.Control
+                        as = "textarea"
+                        rows = {3}
+                        placeholder = "Enter the tems you will include in your menu"
+                        value = {menuDescription}
+                        onChange = {handleDescriptionChange}
                     />
                 </Form.Group>
                 <Button variant = "primary" onClick = {addMenu}>Add Menu</Button>
             </Form>
 
             <ListGroup className = "mt-4">
-                {menus.map((item, index) => (
-                    <ListGroup.Item key = {index}> {item} </ListGroup.Item>
+                {menus.map((menu, index) => (
+                    <ListGroup.Item key = {index}>
+                        <h5>{menu.name}</h5>
+                        <p>{menu.description}</p>
+                    </ListGroup.Item>
                 ))}
             </ListGroup>
         </Container>
-    );  
-};
-}
+        );  
+    };
+
+    export default MenuList;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,43 @@
+import {Container, Jumbotron, Button } from 'react-bootstrap';
+import './Home.css';
+
 export default function Home(){
     return(
-        <>
-            <h1>This is the Home Page!</h1>
-        </>
-        
-    )
-}
+            <Container className = "home-container mt-5">
+                <div className = "text-center">
+                    <h1 className = "display-4">Welcome to Team Menu App</h1>
+                    <p className = "lead">
+                        Create and store your menus with ease. For any occasion, we have you covered
+                    </p>
+
+                    <div class = "btn-row">
+                        <Button type = "button" class = "btn btn-outline px2" href = "/Add">
+                            Add Menu
+                        </Button>
+                        <Button type = "button" class = "btn btn-outline px2" href = "/Retrieve">
+                            Edit Menu
+                        </Button>
+                    </div>
+                </div>
+               
+                <hr className = "my-4"/>
+
+                <div className = "feature-section">
+                    <div className = "row">
+                        <div className = "col-md-6">
+                            <h2 id = "create-menu">Create Your Menu</h2>
+                            <p>
+                                Easily create and customize menus for all different needs. Save, manage, and edit them effortlessly.
+                            </p>
+                        </div>
+                        <div className = "col-md-6">
+                            <h2>Store your Menus</h2>
+                            <p>
+                                Securely store all your created menus. Access them anytime, anywhere, and make updates as needed.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </Container>        
+    );
+};
