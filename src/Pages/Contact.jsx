83dc827634d83e4082edaad1bfd96a6e139e0ed1
Tehila <tehilahakakian@gmail.com>
2024-06-27 19:35:25 -0400

    Updates contact page and Retrieve front end code

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,33 +1,55 @@
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/row";
-import Col from "react-bootstrap/col";
-import { Accordion } from "react-bootstrap";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
 import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
 
-const picture1 = require('../images/Contact.png');
+// Import your image
+import picture1 from '../images/Contact.png';
 
-export default function Contact(){
-    return(
-        <>
-        <Container>
-            <Row>
-                <Image src={picture1} fluid />
+export default function Contact() {
+    const h3Style = {
+        fontFamily: 'cursive',
+        fontSize: '2rem',
+        color: '#5eb8b8',
+        textAlign: 'center',
+        marginBottom: '1rem'
+    };
+
+    return (
+        <Container className="mt-5">
+            <Row className="mb-4">
+                <Col>
+                    <h1 className="text-center">Contact Us</h1>
+                </Col>
             </Row>
-            <Row className = 'p-5'> 
-                <Accordion>
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>Call us</Accordion.Header>
-                        <Accordion.Body> 410-486-MENU </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>Email Us</Accordion.Header>
-                        <Accordion.Body> MenuMagic@gmail.com </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+            <Row className="mb-4">
+                <Col>
+                    <h3 style={h3Style}>
+                        We are here to fulfill all of your needs at all times
+                    </h3>
+                </Col>
             </Row>
-    </Container>
-        </>
-        
-    )
+            <Row className="justify-content-center mb-4">
+                <Col xs={12} sm={10} md={8} lg={6}>
+                    <Image src={picture1} fluid className="rounded mx-auto d-block" style={{ maxWidth: '100%', height: 'auto' }} />
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Call Us</Accordion.Header>
+                            <Accordion.Body>410-486-MENU</Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Email Us</Accordion.Header>
+                            <Accordion.Body>MenuMagic@gmail.com</Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
--- src/Pages/Retrieve.jsx
+++ src/Pages/Retrieve.jsx
@@ -1,8 +1,49 @@
-export default function Retrieve(){
-    return(
-        <>
-            <h1>This is the Retrieve Page!</h1>
-        </>
-        
-    )
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
+
+export default function Update() {
+    const menu = [
+        { 'Shabbos': ['Challah', 'Soup', 'Chicken'] },
+        { 'SundayDinner': ['Chicken Cutlets', 'Broccoli', 'Rice'] },
+        { 'FamilyBBQ': ['Hot dogs', 'Grilled Chicken', 'Corn', 'French Fries'] }
+    ];
+
+    const [selectedMenu, setSelectedMenu] = useState([]);
+
+    const handleMenuChange = (e) => {
+        const selectedValue = JSON.parse(e.currentTarget.value);
+        const selectedArray = Object.values(selectedValue)[0];
+        setSelectedMenu(selectedArray);
+    };
+
+    return (
+        <div className="container mt-4">
+            <h1>Access your Menus here:</h1>
+
+            <Form className="mb-3">
+                <Form.Group controlId="selectMenu">
+                    <Form.Label>Select a Menu</Form.Label>
+                    <Form.Select
+                        value={JSON.stringify(menu.find(item => Object.keys(item)[0] === selectedMenu[0]))}
+                        onChange={handleMenuChange}
+                    >
+                        <option value={JSON.stringify(menu[0])}>Shabbos</option>
+                        <option value={JSON.stringify(menu[1])}>Sunday Dinner</option>
+                        <option value={JSON.stringify(menu[2])}>Family BBQ</option>
+                    </Form.Select>
+                </Form.Group>
+            </Form>
+
+            <div>
+                {selectedMenu.length > 0 && (
+                    <ul className="list-group">
+                        {selectedMenu.map((item, index) => (
+                            <li key={index} className="list-group-item">{item}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 }
